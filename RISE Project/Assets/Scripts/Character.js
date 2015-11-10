1da3a26da1d7478627d5cf81462390a178c01772
joegrainger <joe@torquestudios.co.uk>
2015-11-10 17:19:11 +0000

    Project Update
    Character Following & Collectables

---
+++ RISE Project/Assets/Scripts/Character.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var worth : int = 2000;
+var animator : Animator;
+var follow : Follow;
+
+public var audioClip: AudioClip;
+private var used : boolean;
+private var audioSource : AudioSource;
+
+function Awake () {
+	audioSource = GetComponent.<AudioSource>();
+}
+
+function OnTriggerEnter (other : Collider) : void { 
+	if(used)
+		return;
+
+	used = true;
+	var scoreManager : ScoreManager = other.GetComponent.<ScoreManager>();
+	audioSource.PlayOneShot(audioClip, 1F);
+	scoreManager.AddScore(worth);
+	follow.Initialise();
+	Remove();
+}
+
+function Remove () : IEnumerator {
+	//animator.SetTrigger("Remove");
+	Destroy(GetComponent.<Collider>());
+}
---
+++ RISE Project/Assets/Scripts/_Testing/Follow.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var target : Transform;
+var followDelay : float = 0.2f;
+var recordedPos : List.<Vector3>;
+var pos : int;
+
+private var running : boolean;
+
+function Initialise () : IEnumerator {
+	if(running)
+		return;
+		
+	running = true;
+	Record();
+	yield WaitForSeconds(followDelay);
+	Follow();
+}
+
+function Record () : IEnumerator {
+	while(true){
+		recordedPos.Add(target.position);	
+		yield;
+	}
+}
+
+function Follow () : IEnumerator {
+	while(true){
+		transform.position.x = Mathf.Lerp(transform.position.x, recordedPos[pos].x, Time.deltaTime*15);
+		transform.position.y = Mathf.Lerp(transform.position.y, recordedPos[pos].y, Time.deltaTime*20);
+		pos++;
+		yield;
+	}
+	running = false;
+}
