0222c998a0086cff35fdf77b7f649bd51798bf97
Phil J. Laszkowicz <phil@fillip.pro>
2016-02-11 21:45:14 +0000

    Added music (Joe Grainger) to game.

---
+++ RISE Project/Assets/Scripts/MusicManager.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var gameMusic : AudioSource;
+var menuMusic : AudioSource;
+
+function Awake () {
+	DontDestroyOnLoad(this);
+}
+
+function OnLevelWasLoaded () : void {
+	if(Application.loadedLevelName == "01Game")
+		Game();
+	else
+		Menu();
+}
+
+function Menu () : void {
+	menuMusic.volume = 0.5f;
+	gameMusic.Stop();
+	menuMusic.Play();
+}
+
+function Game () : void {
+	menuMusic.volume = 0.5f;
+	gameMusic.Play();
+	menuMusic.Stop();
+}
+
+function Pause () : void {
+	menuMusic.volume = 0.1f;
+	gameMusic.Stop();
+	menuMusic.Play();
+}
+
+function Resume () : void {
+	menuMusic.volume = 0.5f;
+	gameMusic.Play();
+	menuMusic.Stop();
+}
--- RISE Project/Assets/Scripts/PauseManager.js
+++ RISE Project/Assets/Scripts/PauseManager.js
@@ -8,7 +8,11 @@ var pauseMenu : GameObject;
 var firstSelected : GameObject;
 
 var eventSystem : UnityEngine.EventSystems.EventSystem;
+var musicManager : MusicManager;
 
+function Awake () : void {
+	musicManager = GameObject.Find("MusicManager").GetComponent.<MusicManager>();
+}
 
 function Pressed () : void {
 	Toggle();
@@ -21,9 +25,11 @@ function Toggle () {
 	eventSystem.SetSelectedGameObject(null);
 	
 	if(paused){
+		musicManager.Pause();
 		eventSystem.SetSelectedGameObject(firstSelected);
 		Time.timeScale = 0f;
 	}else{
+		musicManager.Resume();
 		Time.timeScale = 1f;
 	}
 	
